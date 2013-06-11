3a9c5a942b3346682badf9e23ba0c65c0066aea9
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2013-06-11 13:21:33 -0400

    Allow specifying custom port.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 var server = require('./lib/server');
 module.exports = server;
 
-module.exports.patchClient = function(aws){
+module.exports.patchClient = function(aws, port){
+    port = port || 8080;
+
     aws.config.update({
         'accessKeyId': 'AKID',
         'secretAccessKey': 'SECRET',
         'region': 'us-east-1',
-        'endpoint': 'http://localhost:8080'
+        'endpoint': 'http://localhost:' + port
     });
     return aws;
 };
